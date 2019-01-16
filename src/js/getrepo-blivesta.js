28fecc0dc851f8d3550c8626c60a487d564beee9
blivesta <design@blivesta.com>
2019-01-16 09:59:28 +0900

    tweaks

--- src/js/getrepo-blivesta.js
+++ src/js/getrepo-blivesta.js
@@ -1,13 +1,12 @@
-var $ = require('jquery');
-var icon = require('./icon');
+var $ = require("jquery");
+var icon = require("./icon");
 
 module.exports = function(datas) {
-
   var ranking = 18;
-	var starTotal = 0;
-	var forkTotal = 0;
+  var starTotal = 0;
+  var forkTotal = 0;
   var $projects = $(".js-projects");
-  var $total = $('.js-total');
+  var $total = $(".js-total");
 
   for (var i = 0, length = ranking; i < length; i++) {
     var repo = datas.data.items[i];
@@ -16,47 +15,54 @@ module.exports = function(datas) {
     starTotal = starTotal + repo.stargazers_count;
     forkTotal = forkTotal + repo.forks;
 
-    var detail = repo.language + ' ' + icon.star + repo.stargazers_count + ' ' + icon.fork + repo.forks;
+    var detail =
+      repo.language +
+      " " +
+      icon.star +
+      repo.stargazers_count +
+      " " +
+      icon.fork +
+      repo.forks;
     var homepahge = repo.homepage ? repo.homepage : repo.html_url;
 
-    if(n != 'grunts' && n != 'site-boilerplate' && n != 'gulptasks' && n != 'docs' ){
+    if (
+      n != "grunts" &&
+      n != "site-boilerplate" &&
+      n != "gulptasks" &&
+      n != "docs" &&
+      n != "csscallbacks"
+    ) {
       $projects.append(
-        $("<div>").addClass("Grid-col t-sm-widthHalf t-md-width1of3")
-        .append(
-          $("<a>")
-          .addClass("Card t-clear")
-          .attr('href', homepahge)
-          .attr('target', '_blank')
-            .append(
-              $("<h2>")
-              .addClass("Card-title")
-              .html(repo.name)
-            )
-            .append(
-              $("<ul>")
-              .addClass("ListInline Card-caption t-mbMi")
+        $("<div>")
+          .addClass("Grid-col t-sm-widthHalf t-md-width1of3")
+          .append(
+            $("<a>")
+              .addClass("Card t-clear")
+              .attr("href", homepahge)
+              .attr("target", "_blank")
               .append(
-                $("<li>")
-                .html(repo.language)
+                $("<h2>")
+                  .addClass("Card-title")
+                  .html(repo.name)
               )
               .append(
-                $("<li>")
-                .html(icon.star + '&nbsp;' + repo.stargazers_count)
+                $("<ul>")
+                  .addClass("ListInline Card-caption t-mbMi")
+                  .append($("<li>").html(repo.language))
+                  .append(
+                    $("<li>").html(icon.star + "&nbsp;" + repo.stargazers_count)
+                  )
+                  .append($("<li>").html(icon.fork + "&nbsp;" + repo.forks))
               )
               .append(
-                $("<li>")
-                .html(icon.fork + '&nbsp;' + repo.forks)
+                $("<p>")
+                  .addClass("Card-description")
+                  .html(repo.description)
               )
-            )
-            .append(
-              $("<p>")
-              .addClass("Card-description")
-              .html(repo.description)
-            )
           )
-        );
-      } // if
-    } // for
+      );
+    } // if
+  } // for
 
-    $total.html(starTotal);
+  $total.html(starTotal);
 };
