265a56f14aff62770d261300c76faf669c17f8b4
blivesta <design@blivesta.com>
2019-06-24 22:53:43 +0900

    install gatsby.js

--- src/js/icon.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  star:'<svg class="Icon Icon-star Icon-blivesta"><use xlink:href="#Icon-star" /></svg>',
-  fork:'<svg class="Icon Icon-fork"><use xlink:href="#Icon-fork" /></svg>'
-};
