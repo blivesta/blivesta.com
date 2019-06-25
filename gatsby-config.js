f1359037ecadfa2bcdfdd50f12e2f03a23ffd08f
blivesta <design@blivesta.com>
2019-06-25 22:47:40 +0900

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,27 @@
+const siteUrl = `https://www.blivesta.com`
+
 module.exports = {
   siteMetadata: {
     title: `blivesta`,
     description: `Design engineering and Content storategy.`,
     author: `@blivesta`,
+    siteUrl: siteUrl,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Roboto'],
+        },
+        typekit: {
+          families: ['ITC Avant Garde Gothic Pro Bold'],
+        },
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,21 +32,30 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-21878354-1`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: siteUrl,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `blivesta`,
+        short_name: `blivesta`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#000`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-typescript`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
