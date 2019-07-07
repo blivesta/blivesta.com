3528589f1e25f74f734df7f8d323ef765f2a4144
blivesta <design@blivesta.com>
2019-07-08 00:25:20 +0900

    Fix

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,21 +11,41 @@ const ContactPage = () => {
     <Layout>
       <SEO title="Contact" />
       <ContentBlock id="contact" title="Contact">
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
           <p>
             <label>
-              Your Name: <input type="text" name="name" />
+              Your Name:{' '}
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="お名前"
+                required
+                // autocomplete="name"
+              />
             </label>
           </p>
           <p>
             <label>
-              Your Email: <input type="email" name="email" />
+              Your Email:{' '}
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                placeholder=""
+                pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+                required
+                // autocomplete="email"
+              />
             </label>
           </p>
           <p>
             <label>
-              Message: <textarea name="message"></textarea>
+              Message: <textarea id="meaasage" name="meaasage" required></textarea>
             </label>
           </p>
           <p>
