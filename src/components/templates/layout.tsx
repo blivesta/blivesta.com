c8cf692b4826a6ba59fe197ac24d1095e8e88f12
blivesta <design@blivesta.com>
2019-10-04 00:20:24 +0900

    Fix

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -72,24 +72,22 @@ const Layout = ({ children }: LayoutProps) => {
     }
   `);
 
-  return (
-    typeof window !== `undefined` && (
-      <WebfontLoader config={config} onStatus={fontCallback}>
-        <Wrapper>
-          <GlobalStyles />
-          {state ? (
-            <Contents isActive={state}>
-              <>
-                <Header siteTitle={data.site.siteMetadata.title} description={data.site.siteMetadata.description} />
-                <Main>{children}</Main>
-                <Footer siteTitle={data.site.siteMetadata.title} />
-              </>
-            </Contents>
-          ) : null}
-        </Wrapper>
-      </WebfontLoader>
-    )
-  );
+  return typeof window !== `undefined` ? (
+    <WebfontLoader config={config} onStatus={fontCallback}>
+      <Wrapper>
+        <GlobalStyles />
+        {state ? (
+          <Contents isActive={state}>
+            <>
+              <Header siteTitle={data.site.siteMetadata.title} description={data.site.siteMetadata.description} />
+              <Main>{children}</Main>
+              <Footer siteTitle={data.site.siteMetadata.title} />
+            </>
+          </Contents>
+        ) : null}
+      </Wrapper>
+    </WebfontLoader>
+  ) : null;
 };
 
 export default Layout;
