adb8de09dfcf03b39a94076dec0abe72365193cd
blivesta <design@blivesta.com>
2019-10-24 11:48:39 +0900

    Use @storybook/addon-storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -30,6 +30,17 @@ module.exports = ({ config }) => {
     },
   });
 
+  config.module.rules.push({
+    test: /\.stories\.(ts|tsx|js|jsx)$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' },
+      },
+    ],
+    enforce: 'pre',
+  });
+
   config.resolve.extensions.push('.ts', '.tsx');
 
   return config;
