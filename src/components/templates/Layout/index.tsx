f3589e1aeb00611ce7f814a7193b1a3663b7bf5e
blivesta <design@blivesta.com>
2019-10-26 22:06:34 +0900

    Fix

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-import { useSiteMetadata } from '../../../hooks/use-site-metadata';
+import { useSiteMetadata } from '../../../hooks/useSiteMetadata';
 import GlobalStyle from '../../../styles/global-style';
 import Header from '../../organisms/Header';
 import Footer from '../../organisms/Footer';
