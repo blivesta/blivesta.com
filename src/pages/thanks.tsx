e965c2ad55801b7c874653987f206a4b2eb3e018
blivesta <design@blivesta.com>
2019-11-02 15:31:20 +0900

    Fix contact

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -4,13 +4,16 @@ import Layout from '../components/templates/Layout';
 import Seo from '../components/parts/Seo';
 import ContentBlock from '../components/molecules/ContentBlock';
 import Title from '../components/atoms/Title';
+import P from '../components/atoms/P';
 
 const ThanksPage = () => {
   return (
     <Layout>
-      <Seo pageTitle="Thanks" pageLocation="/thanks/" />
-      <ContentBlock id="thanks" title="Thanks">
-        <Title>Thanks</Title>
+      <Seo pageTitle="Thank you for contact" pageLocation="/thanks/" />
+      <ContentBlock id="thanks" title="Send complete">
+        <Title>Thank you for contact</Title>
+        <P lang="en">The message has been sent successfully. We will contact you as soon as possible. Thank you.</P>
+        <P lang="ja">送信が完了しました。できるだけ早くご連絡いたします。ありがとうございました。</P>
       </ContentBlock>
     </Layout>
   );
