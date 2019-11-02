79db8638c31b192d35f20e1810d29fb10fd30291
blivesta <design@blivesta.com>
2019-11-02 18:38:36 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-preset-gatsby/typescript',
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: ['<rootDir>/setupEnzyme.js'],
+  setupFilesAfterEnv: ['./setupEnzyme.js'],
 };
