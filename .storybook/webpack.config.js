a889cbf028ae5ff4463c0abd32f89423fad716a1
blivesta <design@blivesta.com>
2019-11-07 23:23:04 +0900

    Fix storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,23 +1,19 @@
 module.exports = ({ config }) => {
-  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+  // Gatsby
   config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
-  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
   config.module.rules[0].use[0].loader = require.resolve('babel-loader');
-  // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
     require.resolve('@babel/preset-react'),
     require.resolve('@babel/preset-env'),
   ];
   config.module.rules[0].use[0].options.plugins = [
-    // use @babel/plugin-proposal-class-properties for class arrow functions
     require.resolve('@babel/plugin-proposal-class-properties'),
-    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
     require.resolve('babel-plugin-remove-graphql-queries'),
   ];
-  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
   config.resolve.mainFields = ['browser', 'module', 'main'];
 
   // Typescript
+  config.resolve.extensions.push('.ts', '.tsx');
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
@@ -25,7 +21,6 @@ module.exports = ({ config }) => {
       presets: [['react-app', { flow: false, typescript: true }]],
       plugins: [
         require.resolve('@babel/plugin-proposal-class-properties'),
-        // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
         require.resolve('babel-plugin-remove-graphql-queries'),
       ],
     },
@@ -39,7 +34,18 @@ module.exports = ({ config }) => {
     enforce: 'pre',
   });
 
-  config.resolve.extensions.push('.ts', '.tsx');
+  // SVG
+  config.resolve.extensions.push('.svg');
+  config.module.rules = config.module.rules.map(data => {
+    if (/svg\|/.test(String(data.test)))
+      data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+    return data;
+  });
+  config.module.rules.push({
+    test: /\.svg$/,
+    loader: require.resolve('svg-react-loader'),
+    exclude: /node_module/,
+  });
 
   return config;
 };
